35030f1c0e75004847bc3c8293a6b81c0bbfe51b
Igor Popov <popov.nirvana@gmail.com>
2023-07-08 09:44:00 +0300

    Adjust Build.ps1

--- Build.ps1
+++ Build.ps1
@@ -1,9 +1,9 @@
 $ErrorActionPreference = "Stop"
 
-msbuild -m -p:Platform="x86" -p:Configuration="Debug"
-msbuild -m -p:Platform="x86" -p:Configuration="Release"
-msbuild -m -p:Platform="x64" -p:Configuration="Debug"
-msbuild -m -p:Platform="x64" -p:Configuration="Release"
+msbuild -target:Rebuild -m -p:Platform="x86" -p:Configuration="Debug"
+msbuild -target:Build -m -p:Platform="x86" -p:Configuration="Release"
+msbuild -target:Rebuild -m -p:Platform="x64" -p:Configuration="Debug"
+msbuild -target:Build -m -p:Platform="x64" -p:Configuration="Release"
 
 cd NuGet
 nuget pack idlfe.nuspec
